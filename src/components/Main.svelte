63f7e6ee5a69ab17093e7f99059dfc3a39563747
Anna Gan <annaxjgan@gmail.com>
2024-09-04 12:33:26 +1000

    updated about me section

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,12 +1,6 @@
 <script>
     import Step from "./Step.svelte"
 
-    let benefits =[
-        {name:'Master of IT Student', description:'Currently studying Master of IT, Computing at the University of Melbourne'},
-        {name:'Data Product Consultant', description:'I have a year of work experience as a Data Consultant in a Software Engineering team.'},
-        {name:'Front-end developer', description:'I currently volunteer as a front end developer for a student initiative project, building an application that automates clinical consultation transcription.'},
-    ]
-
     let steps=[
         {name: 'Personal Website', icon: 'fa-solid fa-user-tie'},
         {name: 'Friendship matching app', icon: 'fa-solid fa-mobile-screen-button'},
@@ -17,17 +11,15 @@
     <section id="introPage" class="g:grid-cols-2 gap-10 py-8 sm:py-14 flex flex-col ">
         <div class="flex flex-col items-center justify-center text-center gap-6 md:gap-8 lg:gap-10 lg:h-auto min-h-screen">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
-                Hi! I'm <span class="poppins" style="color: #92d3f7;">Anna Gan</span> <br/><br/>Aspiring <span class="poppins"  style="color: #92d3f7;">Software Developer</span>
+                Hi ! I'm <span class="poppins" style="color: #92d3f7;">Anna.</span> 
+            </h2>
+            <br/><br/>
+            <h2 class="font-semibold text-2xl sm:text-3xl md:text-4xl">
+                I enjoy creating <span class="poppins"  style="color: #92d3f7;"> fun </span> stuff with <span class="poppins"  style="color: #92d3f7;"> Tech </span>
             </h2>
             <p class="text-base sm:text-lg md:text-xl">
-                I enjoy <span class="poppins" style="color: #92d3f7">creating</span> things with <span class="poppins" style="color: #92d3f7">tech</span>
+                Passionate about <span class="poppins" style="color: #92d3f7">Software Development</span> and <span class="poppins" style="color: #92d3f7">Cloud Solutions</span>
             </p>
-            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer" 
-                href="https://www.linkedin.com/in/annagan/" 
-                target="_blank">
-                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 dduration-200"></div>
-                <h4 class="relative z-9">Get in touch &rarr;</h4>
-            </a>
         </div>
 
         <section id="about" class="grid grid-cols-1 top-20 lg:grid-cols-3 gap-10 py-8 sm:py-14 items-center justify-center gap-24 lg:h-auto min-h-screen">
@@ -35,44 +27,49 @@
                 <img src={"images/Profile Picture.png"} alt="Profile Image" class="object-cover z-[2] max-h-[70vh]"/>
             </div>
             <div class="flex flex-col gap-6 md:gap-8 lg:gap-10 col-span-2">
-                <div class="text-center">
-                    <h5 class="font-semibold text-2xl sm:text-3xl md:text-4xl">
-                        <span class="poppins" style="color: #ffffff;">About Me</span> 
-                    </h5>
-                </div>
-                <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
-                    {#each benefits as benefit, index}
-                        <div class="flex gap-6 sm:gap-8">
-                            <p class="poppins text-2xl sm:text-3xl md:text-4xl text-slate-500 font-semibold">
-                                0{index+1}
-                            </p>
-                            <div class="flex flex-col gap-6 sm:gap-8">
-                                <h5 class="text-xl sm:text-2xl md:text-3xl">
-                                    {benefit.name}
-                                </h5>
-                                <p class="text-xl sm:text-2xl md:text-3xl">{benefit.description}</p>
-                            </div>
-                        </div>
-                    {/each}
+                <div>
+                    <p class="poppins">
+                        As an enthusiastic learner, I am always actively learning new skills. 
+                        I am proficient in a variety of programming languages including <b>Java, Python, C# and JavaScript.</b>
+                        <br>
+                        <br>
+                        <h7 class="font-semibold">
+                            <span style="color: #92d3f7;" class="text-sm sm:text-lg md:text-xl">A little background...</span>
+                        </h7>
+                        <br><br>
+                        I studied Chemistry with Molecular Physics at Imperial College London.
+                        Realising that scientific research was not the right fit for me, I joined Kubrick Group as a data consultant upon graduating.
+                        <br> 
+                        <br>
+                        During my work as a consultant, I was placed as a technical business analyst at AstraZeneca within a software engineering team. 
+                        This experience became the catalyst for my interest in software development and the tech field.
+                        <br>
+                        <br>
+                        I am currently a First-Year Master's student in IT, Computing at the Univeristy of Melbourne. I am always on the go — 
+                        constantly seeking opportunities to work on interesting projects to refine my skills.
+                        <br>
+                        <br>
+                        In my free time, I enjoy playing tennis (albeit not being good at it), drawing, exploring the city, and indulging in occassional daydreaming.
+                    </p>
                 </div>
             </div>
         </section>
 
     <section id="projects" class="py-20 lg:py-32 gap-24 lg:h-auto min-h-screen">
         <div class="text-center flex flex-col gap-2 h-[150px]">
-            <h6 class="text-lg sm:text-xl md:text-2xl">
-                Projects I've done.
+            <h6 class="font-semibold text-2xl sm:text-3xl md:text-4xl">
+                My <span style="color: #92d3f7;">Projects</span>
             </h6>
         </div>
         <div class="flex flex-wrap gap-12">
             <div class="flex-1 min-w-[calc(50%-1.5rem)] h-[200px]">
                 <Step step={steps[0]}>
-                    <p>This is the website you're viewing right now. <br/>Created with <strong style="color: #92d3f7;">HTML, CSS, JavaScript & Svelte</strong></p>
+                    <p>My personal website. <br/>Built using: <strong style="color: #92d3f7;">HTML, CSS, JavaScript & Svelte</strong></p>
                 </Step>
             </div>
             <div class="flex-1 min-w-[calc(50%-1.5rem)] h-[200px]">
                 <Step step={steps[1]}>
-                    <p>Friendship app is a mobile app that matches friends of the same interests.<br/>Created with <strong style="color: #92d3f7;">Python, Java, Flask, Figma & AndroidStudio</strong></p>
+                    <p>Friendship app is a mobile app that matches friends of the same interests.<br/>Built using: <strong style="color: #92d3f7;">Python, Java, Flask, Figma & AndroidStudio</strong></p>
                 </Step>
             </div>
 
