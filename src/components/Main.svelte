d80504347f0908fc2cb7614bc8aee54f9b8bbee0
Anna Gan <annaxjgan@gmail.com>
2024-09-04 15:32:02 +1000

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <main class="flex flex-col flex-1 p-4">
-    <section id="introPage" class="g:grid-cols-2 gap-10 py-8 sm:py-14 flex flex-col ">
+    <section id="introPage" class="g:grid-cols-2 gap-10 py-8 sm:py-14 flex flex-col fade-in">
         <div class="flex flex-col items-center justify-center text-center gap-6 md:gap-8 lg:gap-10 min-h-screen">
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi ! I'm <span class="poppins" style="color: #92d3f7;">Anna.</span> 
